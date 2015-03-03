ce4b0ade6a33fe0e8b34c3850250146eda814730
ps.fedorov@gmail.com <ps.fedorov@gmail.com@e0bb78e6-e56d-6c37-2be4-859d8a785cc5>
2015-03-03 10:17:12 +0000

    Added "Block Comment" command.

---
+++ trunk/OSDCodeTemplateCaret/src/none/psfedorov/osdcodetemplatecaret/BlockCommentCommand.java
@@ -0,0 +1,40 @@
+package none.psfedorov.osdcodetemplatecaret;
+
+import oracle.dbtools.worksheet.editor.Worksheet;
+
+import oracle.ide.Ide;
+import oracle.ide.ceditor.CodeEditor;
+import oracle.ide.controller.Command;
+import oracle.ide.extension.RegisteredByExtension;
+import oracle.ide.view.View;
+
+import oracle.javatools.editor.BasicEditorPane;
+
+@RegisteredByExtension("none.psfedorov.osdcodetemplatecaret")
+public class BlockCommentCommand extends Command {
+    
+    public static final String ACTION_ID = "none.psfedorov.osdcodetemplatecaret.BlockComment";
+    
+    public BlockCommentCommand() {
+        super(Ide.findCmdID(ACTION_ID));
+    }
+
+
+
+    @Override
+    public int doit() throws Exception {
+        View view = getContext().getView();
+        if (!(view instanceof Worksheet) && !(view instanceof CodeEditor)) {
+            return OK;
+        }
+        BasicEditorPane pane = (view instanceof Worksheet) ? ((Worksheet) view).getFocusedEditorPane() : ((CodeEditor) view).getFocusedEditorPane();
+        String selectedText = pane.getSelectedText();
+        if (selectedText != null) {
+            pane.replaceSelection((selectedText.startsWith("/*") && selectedText.endsWith("*/"))
+                ? selectedText.substring(2, selectedText.length() - 2)
+                : "/*" + selectedText + "*/"
+            );
+        }
+        return OK;
+    }
+}
